7a7e74425b63d45212c7d23f2a841b06dc0c8ad8
ortizpalanques1 <marco.ortizpalanques@gmail.com>
2020-04-10 10:45:06 +0200

    version faltan nombres partidos

--- arreglarFichero.R
+++ arreglarFichero.R
@@ -1,38 +1,38 @@
----
-title: "Manual"
-author: "Marco Ortiz Palanques"
-date: "4/9/2020"
-output: html_document
----
+#Librerias
+library(readr)
+library(dplyr)
+library(tidyr)
+#Crear columnas en el fichero 03
+X03021911$partidos <- substr(X03021911$X1,9,14)
+X03021911$nombreCorto <- substr(X03021911$X1,15,64)
+X03021911$nombreLargo <- substr(X03021911$X1,65,214)
+#Crear columnas en el fichero 09
+X09021911$localidad <- substr(X09021911$X1,10,16)
+X09021911$distrito <- substr(X09021911$X1,17,18)
+X09021911$seccion <- substr(X09021911$X1,19,22)
+X09021911$mesa <- substr(X09021911$X1,23,23)
+X09021911$censoINE <- substr(X09021911$X1,24,30)
+X09021911$blancos <- substr(X09021911$X1,66,72)
+X09021911$nulos <- substr(X09021911$X1,73,79)
+X09021911$validos <- substr(X09021911$X1,80,86)
+X09021911 <- X09021911[,3:9]
+X09021911$censoINE <- as.numeric(X09021911$censoINE)
+X09021911$blancos <- as.numeric(X09021911$blancos)
+X09021911$nulos <- as.numeric(X09021911$nulos)
+X09021911$validos <- as.numeric(X09021911$validos)
+#Crear columnas fichero 10
+X10021911$localidad <- substr(X10021911$X1,10,16)
+X10021911$distrito <- substr(X10021911$X1,17,18)
+X10021911$seccion <- substr(X10021911$X1,19,22)
+X10021911$mesa <- substr(X10021911$X1,23,23)
+X10021911$partidos <- substr(X10021911$X1,24,29)
+X10021911$votos <- substr(X10021911$X1,30,36)
+X10021911$votos <- as.numeric(X10021911$votos)
+#Extraer el municipio de interés
+getafe20191110 <- X10021911 %>%
+  filter(localidad=="1228065") %>%
+  mutate(ID=paste0(distrito,seccion)) %>%
+  select(3:8) %>%
+  pivot_wider(names_from=partidos, values_from=votos) %>%
+  group_by(ID)
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-## Leer archivos  
-Los archivos se bajan en la página del Ministerio del Interior.
-Los archivos que se leerán serán 03, 09 y 10
-Una vez descomprimidos podemos guardarlos en un directorio. Los archivos con la data vienen numerados del 01 al 10 y tienen la extensión .DAT. Hay además otros dos archivos con las instrucciones.
-Para abrir el archivo en RStudio como un data frame vamos a File> Import Dataset> From Text (readr)... Allí, seleccionamos el archivo a descargar y desmarcamos la opción "First row as names". Para el archivo 03 hay que seleccionar, además, locale> onfigure> encoding> ASCII y luego apretar "Configure" en esa ventana. Como R no acepta que los nombres de los data frames comiencen con números, el nombre sugerido comenzará con "X" más el nombre original del archivo.
-
-## Seleccionar el municipio  
-
-
-
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
-
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-```{r cars}
-summary(cars)
-```
-
-## Including Plots
-
-You can also embed plots, for example:
-
-```{r pressure, echo=FALSE}
-plot(pressure)
-```
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
